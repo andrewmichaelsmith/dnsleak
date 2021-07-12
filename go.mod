de1e99b758e8207be88e2be2354813871e8aba16
Andy Smith <me@andrewmichaelsmith.com>
2021-07-12 21:18:52 +0100

    it connects to es and posts and empty record :tada:

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/andrewmichaelsmith/dnsleak
 
 go 1.14
 
-require github.com/miekg/dns v1.1.42
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/miekg/dns v1.1.42
+	github.com/olivere/elastic v6.2.35+incompatible
+	github.com/olivere/elastic/v7 v7.0.26 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
-	"github.com/miekg/dns"
+	"context"
+	"crypto/tls"
 	"log"
+	"net"
+	"net/http"
+	"net/url"
 	"strconv"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/miekg/dns"
+	"github.com/olivere/elastic"
 )
+
+// Tweet is a structure used for serializing/deserializing data in Elasticsearch.
+type Record struct {
+	date     time.Time `json:"date"`
+	query    string    `json:"query"`
+	sourceIP string    `json:"source-ip"`
+}
+
+type Config struct {
+	Elastic struct {
+		Port        string `envconfig:"ES_PORT" default:"9200" required:"true"`
+		Host        string `envconfig:"ES_HOST" default:"localhost" required:"true"`
+		Username    string `envconfig:"ES_USERNAME"`
+		Password    string `envconfig:"ES_PASSWORD"`
+		VerifyCerts bool   `envconfig:"ES_VERIFY_CERTS" default:"true"`
+	}
+}
 
 func parseQuery(m *dns.Msg) {
 
 	for _, q := range m.Question {
-		log.Printf("Query for %s\n", q.Name)
+		log.Printf("Query for %s %d %d\n", q.Name, q.Qclass, q.Qtype)
 	}
 
 }
@@ -29,6 +55,48 @@ func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
 }
 
 func main() {
+
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	url := url.URL{
+		Scheme: "https",
+		Host:   net.JoinHostPort(cfg.Elastic.Host, cfg.Elastic.Port),
+	}
+
+	ctx := context.Background()
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: !cfg.Elastic.VerifyCerts},
+	}
+	httpClient := &http.Client{Transport: tr}
+
+	client, err := elastic.NewClient(
+		elastic.SetHttpClient(httpClient),
+		elastic.SetURL(url.String()),
+		elastic.SetSniff(false),
+		elastic.SetBasicAuth(cfg.Elastic.Username, cfg.Elastic.Password),
+	)
+
+	if err != nil {
+		log.Fatalf("Failed to connect to es: %s\n ", err.Error())
+	}
+
+	record := Record{date: time.Now(), query: "", sourceIP: ""}
+	_, err = client.Index().
+		Index("dnsleak").
+		Type("log").
+		BodyJson(record).
+		Do(ctx)
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	//client.index
 	// attach request handler func
 	dns.HandleFunc(".", handleDnsRequest)
 
@@ -36,7 +104,7 @@ func main() {
 	port := 5354
 	server := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp"}
 	log.Printf("Starting at %d\n", port)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	defer server.Shutdown()
 	if err != nil {
 		log.Fatalf("Failed to start server: %s\n ", err.Error())
